cbf5704d6516ce41b7ba0025e4610852b1130c3a
bubbleboy14 <mario.balibrera@gmail.com>
2019-01-20 11:54:34 -0800

    Resource model simplification

--- ctvu/model.py
+++ ctvu/model.py
@@ -1,6 +1,5 @@
 from cantools import db
 
 class Resource(db.TimeStampedBase):
-	variety = db.String(choices=["image", "background", "sound"])
 	name = db.String()
-	item = db.Binary(unique=True)
+	item = db.Binary()
