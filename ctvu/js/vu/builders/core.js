08af43e6a385278524093eb17b166441fb525548
bubbleboy14 <mario.balibrera@gmail.com>
2019-01-27 20:24:21 -0800

    builders.core.init(): support noRoom arg

--- ctvu/js/vu/builders/core.js
+++ ctvu/js/vu/builders/core.js
@@ -1,9 +1,10 @@
 vu.builders.current = {};
 
 vu.builders.core = {
-	init: function() {
+	init: function(noRoom) {
 		var _ = vu.builders[location.pathname.split("/").pop().split(".")[0]]._;
-		core.config.ctzero.room = vu.core.room();
+		if (!noRoom)
+			core.config.ctzero.room = vu.core.room();
 		zero.core.util.init();
 		zero.core.util.join(_.raw, _.joined);
 	}
