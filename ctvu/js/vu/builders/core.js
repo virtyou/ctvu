859ea29be09de1109ff8d6efac31eb346ec8bfb6
bubbleboy14 <mario.balibrera@gmail.com>
2019-01-27 19:35:29 -0800

    builders.core.init(): set room (use vu.core.room())

--- ctvu/js/vu/builders/core.js
+++ ctvu/js/vu/builders/core.js
@@ -3,6 +3,7 @@ vu.builders.current = {};
 vu.builders.core = {
 	init: function() {
 		var _ = vu.builders[location.pathname.split("/").pop().split(".")[0]]._;
+		core.config.ctzero.room = vu.core.room();
 		zero.core.util.init();
 		zero.core.util.join(_.raw, _.joined);
 	}
