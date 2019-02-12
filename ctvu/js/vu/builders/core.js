2c834508f95c9c59d2e5af1fa76815e31f89f238
bubbleboy14 <mario.balibrera@gmail.com>
2019-02-11 22:49:38 -0800

    builders.core.init(): support noPerson arg

--- ctvu/js/vu/builders/core.js
+++ ctvu/js/vu/builders/core.js
@@ -1,11 +1,12 @@
 vu.builders.current = {};
 
 vu.builders.core = {
-	init: function(noRoom) {
+	init: function(noPerson, noRoom) {
 		var _ = vu.builders[location.pathname.split("/").pop().split(".")[0]]._;
 		if (!noRoom)
 			core.config.ctzero.room = vu.core.room();
 		zero.core.util.init();
-		zero.core.util.join(_.raw, _.joined);
+		if (!noPerson)
+			zero.core.util.join(_.raw, _.joined);
 	}
 };
