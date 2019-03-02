dc2f15d979763ab667a34fadb3827dfb8ad58450
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-02 01:42:12 -0800

    /_vu request handler: support import action

--- ctvu/_vu.py
+++ ctvu/_vu.py
@@ -3,8 +3,17 @@ from ctone.spawners import person
 from model import db
 
 def response():
-	action = cgi_get("action", choices=["person"])
+	action = cgi_get("action", choices=["person", "import"])
 	if action == "person":
 		succeed(person(cgi_get("name"), cgi_get("owner")).data())
+	if action == "import":
+		pfrom = db.get(cgi_get("from"))
+		pto = db.get(cgi_get("to"))
+		pfbod = pfrom.body.get()
+		ptbod = pto.body.get()
+		for item in ["vibe", "gestures", "dances", "responses"]:
+			setattr(pto, item, getattr(pfrom, item))
+		ptbod.morphs = pfbod.morphs
+		db.put_multi([pto, ptbod])
 
 respond(response)
