7a7510759a973693c7508f3b156e92c9ef3fa82b
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-02 01:04:25 -0800

    init config: added _vu.py to syms{} and routes{}

--- ctvu/init.py
+++ ctvu/init.py
@@ -1,9 +1,13 @@
 syms = {
+	".": ["_vu.py"],
 	"js": ["vu"],
 	"css": ["vu.css"],
 	"html": ["vu"]
 }
 model = {
 	"ctvu.model": ["*"]
+}
+routes = {
+	"/_vu": "_vu.py"
 }
 requires = ["virtyou/ctone"]
