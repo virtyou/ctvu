82582ad7d2dcf561cd2dea7c0dc2e0e3c649f6f1
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-04 22:44:17 -0800

    item page loader

---
+++ ctvu/js/vu/pages/item.js
@@ -0,0 +1,27 @@
+CT.require("CT.align");
+CT.require("CT.data");
+CT.require("CT.dom");
+CT.require("CT.file");
+CT.require("CT.layout");
+CT.require("CT.modal");
+CT.require("CT.parse");
+CT.require("CT.storage");
+CT.require("CT.trans");
+CT.require("core");
+CT.require("user.core");
+CT.require("zero.core");
+CT.require("vu.core");
+CT.require("vu.storage");
+CT.require("vu.builders.core");
+CT.require("vu.builders.item");
+
+CT.onload(function() {
+	CT.initCore();
+	vu.core.init();
+	vu.storage.init(function() {
+		// menu
+		CT.dom.setContent("menu", vu.builders.item.menu());
+		// virtual world
+		vu.builders.core.init(true);
+	});
+});
