11707a2b33e4860c5e205b94139787f01f913397
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-24 15:31:27 -0700

    controls.initCamera(): support gesture/zone/play modes

--- ctvu/js/vu/controls.js
+++ ctvu/js/vu/controls.js
@@ -1,16 +1,56 @@
 vu.controls = {
-	initCamera: function(node) {
-		zero.core.camera.unfollow();
-		var butt = CT.dom.button("far", function() {
-			if (butt.innerHTML == "far") {
-				butt.innerHTML = "near";
-				zero.core.camera.move({ z: 280 });
-			} else {
-				butt.innerHTML = "far";
-				zero.core.camera.move({ z: 120 });
-			}
-		});
-		CT.dom.setContent(node, butt);
+	initCamera: function(node, cb) {
+		var mode = location.pathname.split("/").pop().split(".")[0];
+		if (["zone", "play"].indexOf(mode) != -1) {
+			var cycbutt = CT.dom.button("cycle", function() {
+				if (cycbutt._cycler) {
+					clearInterval(cycbutt._cycler);
+					delete cycbutt._cycler;
+					cycbutt.innerHTML = "cycle";
+				} else {
+					cycbutt._cycler = setInterval(zero.core.current.room.cut, 3000);
+					cycbutt.innerHTML = "stop cycling";
+				}
+			}), pov = CT.dom.button("pov", function() {
+				zero.core.camera.setSprings(200);
+				zero.core.camera.perspective(zero.core.current.person);
+			}), room, tbutts;
+			node.update = function() {
+				room = zero.core.current.room;
+				tbutts = [cycbutt];
+				(mode == "zone") && tbutts.push(CT.dom.button("refresh", function() {
+					room.updateCameras();
+					node.update();
+					vu.builders.zone.persist({
+						cameras: room.cameras
+					});
+				}));
+				CT.dom.setContent(node, [
+					CT.dom.div(tbutts, "right up20"),
+					CT.dom.div([pov].concat(room.cameras.map(function(cam, i) {
+						return CT.dom.button("cam " + i, function() {
+							zero.core.camera.setSprings(20);
+							zero.core.camera.perspective();
+							room.cut(i);
+						});
+					})), "centered clearnode")
+				]);
+			};
+			(mode == "play") && node.update();
+		} else {
+			zero.core.camera.unfollow();
+			var butt = CT.dom.button("far", function() {
+				if (butt.innerHTML == "far") {
+					butt.innerHTML = "near";
+					zero.core.camera.move({ z: 280 });
+				} else {
+					butt.innerHTML = "far";
+					zero.core.camera.move({ z: 120 });
+				}
+				cb && cb(butt.innerHTML);
+			});
+			CT.dom.setContent(node, butt);
+		}
 	},
 	setTriggers: function(node) {
 		var person = zero.core.current.person,
