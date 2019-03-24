9027ea72abd50adb13cc96710031ade2a4e1bf19
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-24 13:30:17 -0700

    basic play builder w/ control menus a la test page

---
+++ ctvu/js/vu/builders/play.js
@@ -0,0 +1,32 @@
+vu.builders.play = {
+	_: {
+		opts: core.config.ctvu.builders.person,
+		selectors: {},
+		menus: {
+			camera: "top",
+			triggers: "bottomleft",
+			gestures: "bottomright"
+		},
+		joined: function(person) {
+			var _ = vu.builders.play._;
+			vu.builders.current.person = zero.core.current.person = person;
+			vu.controls.initCamera(_.selectors.camera);
+			vu.controls.setTriggers(_.selectors.triggers);
+			vu.controls.setGestures(_.selectors.gestures);
+		},
+		setup: function() {
+			var _ = vu.builders.play._, selz = _.selectors,
+				popts = _.opts = vu.storage.get("person") || _.opts;
+			_.raw = vu.core.person(popts);
+			selz.camera = CT.dom.div(null, "centered");
+			selz.triggers = CT.dom.div();
+			selz.gestures = CT.dom.div();
+		}
+	},
+	menus: function() {
+		var section, _ = vu.builders.play._, selz = _.selectors;
+		_.setup();
+		for (section in _.menus)
+			vu.core.menu(section, _.menus[section], selz[section]).show("ctmain");
+	}
+};
