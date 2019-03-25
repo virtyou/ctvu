97a326969f646cfc8e68aea93a00710fff8efa7a
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-24 19:27:51 -0700

    play builder, _: action() -- rooms are now portal-connected!

--- ctvu/js/vu/builders/play.js
+++ ctvu/js/vu/builders/play.js
@@ -15,7 +15,27 @@ vu.builders.play = {
 			vu.controls.setGestures(_.selectors.gestures);
 			person.body.setBounds();
 			_.controls = new zero.core.Controls({
+				cb: _.action,
 				target: person
+			});
+		},
+		action: function() {
+			var person = zero.core.current.person,
+				pos = person.body.bone.position;
+			zero.core.current.room.objects.filter(function(obj) {
+				var o = obj.opts, og = o.portals && o.portals.outgoing;
+				return og && og.target;
+			}).forEach(function(portal) {
+				var dist = portal.position().distanceTo(pos);
+				CT.log(portal.name + " " + dist);
+				if (dist < 100) {
+					CT.db.one(portal.opts.portals.outgoing.target, function(target) {
+						if (target.owner) // room
+							person.say("this door is locked");
+						else
+							zero.core.util.room(CT.data.get(target.parent));
+					}, "json");
+				}
 			});
 		},
 		setup: function() {
