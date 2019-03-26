f128346ac9fcbbc1e0b24c6b48e5222cbfc579e9
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-25 21:59:48 -0700

    play builder, chat stuff

--- ctvu/js/vu/builders/play.js
+++ ctvu/js/vu/builders/play.js
@@ -21,9 +21,6 @@ vu.builders.play = {
 				moveCb: vu.live.up
 			});
 		},
-		chat: function() {
-
-		},
 		port: function(target) {
 			var _ = vu.builders.play._, cur = zero.core.current;
 			CT.pubsub.unsubscribe(cur.room.opts.key);
@@ -53,18 +50,40 @@ vu.builders.play = {
 					}, "json");
 				}
 			});
-			if (!hit)
-				_.chat();
+		},
+		chat: function(person, msg) {
+			var mnode = CT.dom.div([
+				CT.dom.span(person.name, "bold italic green"),
+				CT.dom.pad(),
+				CT.dom.span(msg)
+			]);
+			person.say(msg, null, true);
+			CT.dom.addContent(vu.builders.play._.selectors.chat.out, mnode);
+			mnode.scrollIntoView();
 		},
 		setup: function() {
 			var _ = vu.builders.play._, selz = _.selectors,
-				popts = _.opts = vu.storage.get("person") || _.opts;
+				popts = _.opts = vu.storage.get("person") || _.opts,
+				blurs = core.config.ctvu.blurs;
 			_.raw = vu.core.person(popts);
 			selz.cameras = CT.dom.div(null, "centered");
 			selz.triggers = CT.dom.div();
 			selz.gestures = CT.dom.div();
 			selz.run_home = CT.dom.img("/img/home.png", null, function() { _.port(); });
-			selz.chat = CT.dom.div();
+			var out = CT.dom.div(null, "out"), say = function(val) {
+				val && CT.pubsub.publish(zero.core.current.room.opts.key, val);
+				return "clear";
+			}, listButt = CT.dom.button("listen", function(e) {
+				listButt.style.color = "red";
+				zero.core.rec.listen(function(phrase) {
+					say(phrase);
+					listButt.style.color = "black";
+				});
+				e.stopPropagation();
+			}, "right up20"), cbox = CT.dom.smartField(say, "w1 block mt5", null, null, null, blurs.talk);
+			cbox.onclick = function(e) { e.stopPropagation(); };
+			selz.chat = CT.dom.div([ listButt, out, cbox ]);
+			selz.chat.out = out;
 		},
 		collapse: function(section) {
 			var _ = vu.builders.play._, selz = _.selectors,
