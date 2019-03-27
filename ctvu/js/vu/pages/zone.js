91d5912496295dc6ace10c2bc06973698fe7af9e
bubbleboy14 <mario.balibrera@gmail.com>
2019-03-26 21:10:52 -0700

    zone page loader: vu.controls import

--- ctvu/js/vu/pages/zone.js
+++ ctvu/js/vu/pages/zone.js
@@ -13,6 +13,7 @@ CT.require("user.core");
 CT.require("zero.core");
 CT.require("vu.core");
 CT.require("vu.color");
+CT.require("vu.controls");
 CT.require("vu.storage");
 CT.require("vu.builders.core");
 CT.require("vu.builders.zone");
