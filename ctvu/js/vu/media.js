0c3d90f650b834176a30ce71541e931003911f71
bubbleboy14 <mario.balibrera@gmail.com>
2019-04-05 10:37:20 -0700

    media: _ - isMap(), isResource(), fetch(); browse(); viewer(); browse buttons w/ media selector menus

--- ctvu/js/vu/media.js
+++ ctvu/js/vu/media.js
@@ -3,11 +3,50 @@ vu.media = {
 		types: {
 			image: "img",
 			background: "img"
+		},
+		isMap: function(sel) {
+			return ["map", "panorama"].indexOf(sel) != -1;
+		},
+		isResource: function(sel) {
+			return ["image", "background", "audio", "video"].indexOf(sel) != -1;
+		},
+		fetch: function(variety, cb) {
+			if (vu.media._.resources)
+				return cb(vu.media._.resources[variety]);
+			CT.db.get("resource", function(rez) {
+				var rz = vu.media._.resources = {};
+				rez.forEach(function(r) {
+					if (!(r.variety in rz))
+						rz[r.variety] = [];
+					rz[r.variety].push(r);
+				});
+				cb(rz[variety]);
+			});
 		}
+	},
+	browse: function(variety, cb) {
+		vu.media._.fetch(variety, function(resources) {
+			vu.core.choice({
+				data: resources,
+				cb: cb
+			})
+		});
+	},
+	viewer: function(node, opts, sel) {
+		if (sel == "environment") return;
+		if (vu.media._.isMap(sel)) {
+			node.classList.add("h100p");
+			return zero.core.util[sel](opts.item, node);
+		}
+		CT.dom.setContent(node, CT.dom[vu.media._.types[sel] || sel]({
+			src: opts.item,
+			controls: true,
+			className: "w1"
+		}));
 	},
 	selector: function(rez, sel, cb) {
 		var isIframe = sel == "iframe",
-			isMap = ["map", "panorama"].indexOf(sel) != -1,
+			isMap = vu.media._.isMap(sel),
 			opts = rez[sel] || {
 				variety: sel,
 				modelName: "resource"
@@ -15,20 +54,8 @@ vu.media = {
 
 		// viewer (img/audio)
 		var viewer = CT.dom.div(null, "mt5");
-		var setViewer = function() {
-			if (sel == "environment") return;
-			if (isMap) {
-				viewer.classList.add("h100p");
-				return zero.core.util[sel](opts.item, viewer);
-			}
-			CT.dom.setContent(viewer, CT.dom[vu.media._.types[sel] || sel]({
-				src: opts.item,
-				controls: true,
-				className: "w1"
-			}));
-		};
 		if (opts.item)
-			setViewer();
+			vu.media.viewer(viewer, opts, sel);
 
 		// item
 		if (sel == "environment") {
@@ -49,7 +76,7 @@ vu.media = {
 					oi[axis] = parseFloat(val);
 					item.lastElementChild.style.display = "block";
 					if (oi.lat && oi.lng) {
-						setViewer();
+						vu.media.viewer(viewer, opts, sel);
 						cb();
 					}
 				}, "w1 block mt5" + ((i && !oi[axis]) ? " hidden" : ""),
@@ -59,7 +86,7 @@ vu.media = {
 			item = CT.dom.div(CT.file.dragdrop(function(ctfile) {
 				ctfile.upload("/_db", function(url) {
 					opts.item = url;
-					setViewer();
+					vu.media.viewer(viewer, opts, sel);
 					cb();
 				}, {
 					action: "blob",
@@ -69,24 +96,25 @@ vu.media = {
 			}), !opts.item && "hidden");
 		}
 
+		var update = function(resource) {
+			if (!(sel in rez))
+				rez[sel] = opts;
+			if (resource && resource.key)
+				opts.key = resource.key;
+			if (isIframe) {
+				opts.item = val;
+				vu.media.viewer(viewer, opts, sel);
+			} else
+				CT.dom.show(item);
+			cb();
+		};
+
 		// name (required)
 		var name = CT.dom.smartField(function(val) {
 			if (!val) return name.blur();
 			opts.name = val;
-			var medUp = function(resource) {
-				if (!(sel in rez))
-					rez[sel] = opts;
-				if (resource && resource.key)
-					opts.key = resource.key;
-				if (isIframe) {
-					opts.item = val;
-					setViewer();
-				} else
-					CT.dom.show(item);
-				cb();
-			};
 			if (isIframe || isMap || sel == "environment")
-				return medUp();
+				return update();
 			CT.net.post({
 				path: "/_db",
 				params: {
@@ -94,10 +122,22 @@ vu.media = {
 					pw: core.config.ctvu.storage.apikey,
 					data: opts
 				},
-				cb: medUp
+				cb: update
 			});
 		}, null, null, opts.name, null, blurs[sel] || blurs.resource);
 
-		return CT.dom.div([name, item, viewer], !rez[sel] && "hidden");
+		// media selector popup
+		var browse = CT.dom.img("/img/vu/browse.png", "right h30p up5", function() {
+			vu.media.browse(sel, function(res) {
+				rez[sel] = opts = res;
+				name.value = opts.name;
+				vu.media.viewer(viewer, opts, sel);
+				cb();
+			});
+		});
+		if (!vu.media._.isResource(sel))
+			CT.dom.hide(browse);
+
+		return CT.dom.div([browse, name, item, viewer], !rez[sel] && "hidden");
 	}
 };
