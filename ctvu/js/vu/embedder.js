3638493fed67a8f61a8c419c5229153e63e4b4e1
bubbleboy14 <mario.balibrera@gmail.com>
2019-04-07 09:54:39 -0700

    embedder: embed() returns _.person() w/ say(), respond(), key, and iframe; _.pusher() adds cb to person and pushes postMessage() to person.iframe (w/ cb: person.key); _.puller() uses cbkey (passed back as event.data.data); base object rename, vu->VU

--- ctvu/js/vu/embedder.js
+++ ctvu/js/vu/embedder.js
@@ -1,32 +1,41 @@
-window.vu = {
+window.VU = {
 	_: {
-		pusher: function(action) {
+		people: {},
+		pusher: function(action, person) {
 			return function(words, cb) {
-				vu._.cb = cb;
-				vu._.iframe.contentWindow.postMessage({
+				person.cb = cb;
+				person.iframe.contentWindow.postMessage({
 					action: action,
 					data: words,
-					cb: !!cb
+					cb: cb && person.key
 				});
 			};
 		},
 		puller: function(event) {
 			var d = event.data;
 			if (d.action == "cb")
-				vu._.cb();
+				VU._.people[d.data].cb();
 			// else....
+		},
+		person: function(ifr, key) {
+			var p = VU._.people[key] = { iframe: ifr, key: key };
+			p.say = VU._.pusher("say", p);
+			p.respond = VU._.pusher("respond", p);
+			return p;
 		}
 	},
 	embed: function(node, pkey, rkey, domain, protocol) {
-		var ifr = vu._.iframe = document.createElement("iframe");
-		ifr.src = (protocol || "https") + "://" + (domain || "virtyou.org") + "/vu/embed.html#" + pkey + "_" + rkey;
+		VU.init();
+		var ifr = document.createElement("iframe"), key = pkey + "_" + rkey;
+		ifr.src = (protocol || "https") + "://" + (domain || "virtyou.org") + "/vu/embed.html#" + key;
 		ifr.style.width = ifr.style.height = "100%";
 		node.appendChild(ifr);
-		vu.init();
+		return VU._.person(ifr, key);
 	},
 	init: function() {
-		vu.say = vu._.pusher("say");
-		vu.respond = vu._.pusher("respond");
-		window.addEventListener("message", vu._.puller);
+		if (!VU._.initialized) {
+			VU._.initialized = true;
+			window.addEventListener("message", VU._.puller);
+		}
 	}
 };
