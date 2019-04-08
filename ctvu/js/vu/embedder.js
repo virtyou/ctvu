778f8f80c3d7b45f5348d57585dc07f86cd91041
bubbleboy14 <mario.balibrera@gmail.com>
2019-04-07 19:05:25 -0700

    embedder (VU) bridge support: bridge(); _ - bridges{}, queries[], pusher()->sender(), upbridge(), bridge(), iframe()

--- ctvu/js/vu/embedder.js
+++ ctvu/js/vu/embedder.js
@@ -1,42 +1,77 @@
 window.VU = {
 	_: {
 		people: {},
+		bridges: {},
 		actions: ["say", "respond", "responses", "triggers", "trigger", "listen"],
-		pusher: function(action, person) {
+		queries: ["rooms", "people", "room", "person"],
+		sender: function(action, entity, onsend) {
 			return function(data, cb) {
-				if (action == "trigger")
-					person.cbs[data] = cb;
+				if (["trigger", "people", "rooms"].indexOf(action) != -1)
+					entity.cbs[data] = cb;
 				else
-					person.cb = cb;
-				person.iframe.contentWindow.postMessage({
+					entity.cb = cb;
+				entity.iframe.contentWindow.postMessage({
 					action: action,
 					data: data,
 					cb: !!cb
 				});
+				return onsend && onsend(action, entity, data);
 			};
 		},
 		puller: function(event) {
-			var d = event.data, person = VU._.people[d.person];
-			if (d.action == "cb")
-				person.cb(d.data);
-			else if (d.action == "trigger")
-				person.cbs[d.data]();
+			var d = event.data, person;
+			if (d.person) {
+				person = VU._.people[d.person];
+				if (d.action == "cb")
+					person.cb(d.data);
+				else if (d.action == "trigger")
+					person.cbs[d.data]();
+			} else // bridge
+				VU._.bridges[d.bridge].cbs[d.action](d.data);
 		},
 		person: function(ifr, key) {
 			var p = VU._.people[key] = {
 				cbs: {}, key: key, iframe: ifr
 			};
 			VU._.actions.forEach(function(action) {
-				p[action] = vu._.pusher(action, p);
+				p[action] = VU._.sender(action, p);
 			});
 			return p;
+		},
+		upbridge: function(action, entity, data) {
+			if (["room", "person"].indexOf(action) != -1) {
+				entity[action] = data;
+				if (action == "person")
+					return VU._.person(entity.iframe,
+						entity.person + "_" + entity.room);
+			}
+		},
+		bridge: function(ifr, key) {
+			var _ = VU._, b = _.bridges[key] = {
+				cbs: {}, key: key, iframe: ifr
+			};
+			_.queries.forEach(function(query) {
+				b[query] = _.sender(query, b, _.upbridge);
+			});
+			return p;
+		},
+		iframe: function(key, domain, protocol) {
+			var ifr = document.createElement("iframe");
+			ifr.src = (protocol || "https") + "://" + (domain || "virtyou.org") + "/vu/embed.html#" + key;
+			ifr.style.width = ifr.style.height = "100%";
+			return ifr;
 		}
+	},
+	bridge: function(node, ukey, domain, protocol) { // useful for getting user data pre-select
+		VU.init();
+		var ifr = VU._.iframe(ukey, domain, protocol);
+		node.appendChild(ifr);
+		return VU._.bridge(ifr, ukey);
 	},
 	embed: function(node, pkey, rkey, domain, protocol) {
 		VU.init();
-		var ifr = document.createElement("iframe"), key = pkey + "_" + rkey;
-		ifr.src = (protocol || "https") + "://" + (domain || "virtyou.org") + "/vu/embed.html#" + key;
-		ifr.style.width = ifr.style.height = "100%";
+		var key = pkey + "_" + rkey,
+			ifr = VU._.iframe(key, domain, protocol);
 		node.appendChild(ifr);
 		return VU._.person(ifr, key);
 	},
