3e7b66ddbec45f8b04590666a038dfb9ef81eec0
bubbleboy14 <mario.balibrera@gmail.com>
2019-04-08 18:01:02 -0700

    core.udata(): support skipsets arg (no seta()/setz())

--- ctvu/js/vu/core.js
+++ ctvu/js/vu/core.js
@@ -199,10 +199,10 @@ vu.core = {
 		};
 		return vu.core._udata;
 	},
-	udata: function(cb, allrooms, ukey) {
+	udata: function(cb, allrooms, ukey, skipsets) {
 		if (vu.core._udata)
 			return cb(vu.core._udata);
-		if (!user.core.get()) // cfg.access.anon must be true
+		if (!(ukey || user.core.get())) // cfg.access.anon must be true
 			return cb(vu.core.locd());
 		ukey = ukey || user.core.get("key");
 		vu.core.z({
@@ -219,6 +219,8 @@ vu.core = {
 				});
 			}
 			vu.core._udata = data;
+			if (skipsets)
+				return cb(data);
 			vu.core.seta(function() {
 				vu.core.setz();
 				cb(data);
