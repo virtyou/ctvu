961f1e480f259c278ee007f746cf36358bb02def
bubbleboy14 <mario.balibrera@gmail.com>
2019-04-24 18:00:37 -0700

    talk builder: really fixed chaining

--- ctvu/js/vu/builders/talk.js
+++ ctvu/js/vu/builders/talk.js
@@ -46,8 +46,7 @@ vu.builders.talk = {
 						return opts.name;
 					var resp, path, trig, chain;
 					[resp, path, trig] = _.getCluster(opts.id);
-					if (trig && opts.name && trig != opts.name) // ? lol
-						chain = CT.dom.span(resp.branches[trig].branches[opts.name].chain);
+					chain = CT.dom.span(resp.branches[trig].chain);
 					return [
 						CT.dom.span(opts.name),
 						CT.dom.pad(),
@@ -57,7 +56,7 @@ vu.builders.talk = {
 								cb: function(val) {
 									val = val.toLowerCase();
 									if (!val) return;
-									resp.branches[trig].branches[opts.name].chain = val;
+									resp.branches[trig].chain = val;
 									CT.dom.setContent(chain, val);
 									vu.builders.talk.persist({
 										responses: person.opts.responses
