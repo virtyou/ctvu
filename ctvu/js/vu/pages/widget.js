32bff1c9a886ece2a0018287d5ee32282a09c914
bubbleboy14 <mario.balibrera@gmail.com>
2019-05-08 19:53:01 -0700

    widget page loader: user.core import

--- ctvu/js/vu/pages/widget.js
+++ ctvu/js/vu/pages/widget.js
@@ -3,6 +3,7 @@ CT.require("CT.db");
 CT.require("CT.dom");
 CT.require("CT.storage");
 CT.require("core");
+CT.require("user.core");
 CT.require("zero.core");
 CT.require("vu.core");
 CT.require("vu.widget");
