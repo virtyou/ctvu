3970165d2f960a9fd3c7103f1245d9b48ba896a7
bubbleboy14 <mario.balibrera@gmail.com>
2020-04-21 15:13:20 -0700

    mod page loader

---
+++ ctvu/js/vu/pages/mod.js
@@ -0,0 +1,27 @@
+CT.require("CT.align");
+CT.require("CT.data");
+CT.require("CT.dom");
+CT.require("CT.layout");
+CT.require("CT.modal");
+CT.require("CT.parse");
+CT.require("CT.storage");
+CT.require("CT.trans");
+CT.require("core");
+CT.require("user.core");
+CT.require("zero.core");
+CT.require("vu.core");
+CT.require("vu.controls");
+CT.require("vu.storage");
+CT.require("vu.builders.core");
+CT.require("vu.builders.mod");
+
+CT.onload(function() {
+	CT.initCore();
+	vu.core.init();
+	vu.storage.init(function() {
+		// menu
+		vu.builders.mod.menus();
+		// virtual world
+		vu.builders.core.init();
+	});
+});
