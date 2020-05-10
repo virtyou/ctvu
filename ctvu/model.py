cfe2b56cb3911ae5e26f57043d3733ab14dce8b0
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-09 17:03:33 -0700

    model: Scene, Game, Adventure

--- ctvu/model.py
+++ ctvu/model.py
@@ -4,3 +4,24 @@ class Resource(db.TimeStampedBase):
 	variety = db.String(choices=["image", "background", "audio", "video"])
 	name = db.String()
 	item = db.Binary(unique=True)
+
+class Scene(db.TimeStampedBase):
+	name = db.String()
+	description = db.Text()
+	room = db.ForeignKey(kind="Room")
+	actors = db.ForeignKey(kind="Person", repeated=True)
+	props = db.JSON() # state/interactivity map - room items
+	scripts = db.JSON() # trigger "start" on load
+
+class Game(db.TimeStampedBase):
+	name = db.String()
+	description = db.Text()
+	scenes = db.ForeignKey(kind=Scene, repeated=True)
+	initial = db.JSON()
+	victory = db.JSON()
+	defeat = db.JSON()
+
+class Adventure(db.TimeStampedBase):
+	game = db.ForeignKey(kind=Game)
+	players = db.ForeignKey(kind="Person", repeated=True)
+	state = db.JSON() # start @ game.initial{}
