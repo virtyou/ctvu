2883f80de925a693f3f4f90f12a7fd353455d2ee
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-10 16:17:04 -0700

    game page loader

---
+++ ctvu/js/vu/pages/game.js
@@ -0,0 +1,31 @@
+CT.require("CT.align");
+CT.require("CT.data");
+CT.require("CT.db");
+CT.require("CT.dom");
+CT.require("CT.key");
+CT.require("CT.layout");
+CT.require("CT.modal");
+CT.require("CT.parse");
+CT.require("CT.pubsub");
+CT.require("CT.storage");
+CT.require("CT.trans");
+CT.require("core");
+CT.require("user.core");
+CT.require("zero.core");
+CT.require("vu.core");
+CT.require("vu.controls");
+CT.require("vu.storage");
+CT.require("vu.game");
+CT.require("vu.builders.core");
+CT.require("vu.builders.game");
+
+CT.onload(function() {
+	CT.initCore();
+	vu.core.init();
+	vu.storage.init(function() {
+		// menu
+		vu.builders.game.menus();
+		// virtual world
+		vu.builders.core.init();
+	}, true);
+});
