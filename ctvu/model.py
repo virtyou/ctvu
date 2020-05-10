c37f76973d7791ba35fa5dcb0ff38b368be90ccd
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-09 22:48:45 -0700

    model: Scene.owners[]; Game owners[] and players[]; Adventure +owner, -players[] (now on Game), json(); CTUser->Member note

--- ctvu/model.py
+++ ctvu/model.py
@@ -1,11 +1,15 @@
 from cantools import db
 
+# change all owners, CTUser->Member asap!
+# add Resource.owner right??
+
 class Resource(db.TimeStampedBase):
 	variety = db.String(choices=["image", "background", "audio", "video"])
 	name = db.String()
 	item = db.Binary(unique=True)
 
 class Scene(db.TimeStampedBase):
+	owners = db.ForeignKey(kind="CTUser", repeated=True)
 	name = db.String()
 	description = db.Text()
 	room = db.ForeignKey(kind="Room")
@@ -25,14 +29,25 @@ class Scene(db.TimeStampedBase):
 		}
 
 class Game(db.TimeStampedBase):
+	owners = db.ForeignKey(kind="CTUser", repeated=True)
 	name = db.String()
 	description = db.Text()
 	scenes = db.ForeignKey(kind=Scene, repeated=True)
+	players = db.ForeignKey(kind="Person", repeated=True)
 	initial = db.JSON()
 	victory = db.JSON()
 	defeat = db.JSON()
 
 class Adventure(db.TimeStampedBase):
+	owner = db.ForeignKey(kind="CTUser")
 	game = db.ForeignKey(kind=Game)
-	players = db.ForeignKey(kind="Person", repeated=True)
 	state = db.JSON() # start @ game.initial{}
+
+	def json(self):
+		gd = self.game.get().data()
+		gd['players'] = [p.json() for p in db.get_multi(gd['players'])]
+		return {
+			"key": self.id(),
+			"game": gd,
+			"state": self.state
+		}
