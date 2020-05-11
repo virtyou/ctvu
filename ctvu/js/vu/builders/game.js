a8efef84cffe61a5a229471f15ee84d0d183a4c3
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-10 23:27:08 -0700

    game builder: moved create() and load() off of _{}

--- ctvu/js/vu/builders/game.js
+++ ctvu/js/vu/builders/game.js
@@ -1,80 +1,5 @@
 vu.builders.game = {
 	_: {
-		create: function(ctype, cb, extras) {
-			ctype = ctype || "game";
-			CT.modal.prompt({
-				prompt: "what's the new " + ctype + " called?",
-				cb: function(name) {
-					CT.modal.prompt({
-						isTA: true,
-						prompt: "please describe the " + ctype,
-						cb: function(desc) {
-							vu.storage.edit(CT.merge(extras, {
-								modelName: ctype,
-								name: name,
-								description: desc,
-								owners: [user.core.get("key")]
-							}), cb || vu.builders.game._.load);
-						}
-					});
-				}
-			});
-		},
-		load: function(game) {
-			var _ = vu.builders.game._;
-			CT.dom.setContent("ctmain", [
-				CT.dom.div([
-					"name",
-					CT.dom.smartField({
-						classname: "w1",
-						value: game.name,
-						cb: function(val) {
-							game.name = val;
-							vu.storage.edit({
-								key: game.key,
-								name: val
-							});
-
-						}
-					})
-				], "bordered padded margined round"),
-				CT.dom.div([
-					"description",
-					CT.dom.smartField({
-						isTA: true,
-						classname: "w1",
-						value: game.description,
-						cb: function(val) {
-							game.description = val;
-							vu.storage.edit({
-								key: game.key,
-								description: val
-							});
-
-						}
-					})
-				], "bordered padded margined round"),
-				CT.dom.div([
-					"scenes",
-					_.scenes(game)
-				], "bordered padded margined round"),
-				CT.dom.div([
-					"conditions",
-					_.conditions(game)
-				], "bordered padded margined round"),
-				CT.dom.div([
-					"live",
-					CT.dom.checkboxAndLabel("list on games page",
-						game.live, null, null, null, function(cb) {
-							game.live = cb.checked;
-							vu.storage.edit({
-								key: game.key,
-								live: game.live
-							});
-						})
-				], "bordered padded margined round")
-			]);
-		},
 		scene: function(s) {
 			return CT.dom.div([
 				CT.dom.link(s.name, null,
@@ -114,17 +39,91 @@ vu.builders.game = {
 
 		}
 	},
-	init: function() {
+	create: function(ctype, cb, extras) {
+		ctype = ctype || "game";
+		CT.modal.prompt({
+			prompt: "what's the new " + ctype + " called?",
+			cb: function(name) {
+				CT.modal.prompt({
+					isTA: true,
+					prompt: "please describe the " + ctype,
+					cb: function(desc) {
+						vu.storage.edit(CT.merge(extras, {
+							modelName: ctype,
+							name: name,
+							description: desc,
+							owners: [user.core.get("key")]
+						}), cb || vu.builders.game.load);
+					}
+				});
+			}
+		});
+	},
+	load: function(game) {
 		var _ = vu.builders.game._;
+		CT.dom.setContent("ctmain", [
+			CT.dom.div([
+				"name",
+				CT.dom.smartField({
+					classname: "w1",
+					value: game.name,
+					cb: function(val) {
+						game.name = val;
+						vu.storage.edit({
+							key: game.key,
+							name: val
+						});
+
+					}
+				})
+			], "bordered padded margined round"),
+			CT.dom.div([
+				"description",
+				CT.dom.smartField({
+					isTA: true,
+					classname: "w1",
+					value: game.description,
+					cb: function(val) {
+						game.description = val;
+						vu.storage.edit({
+							key: game.key,
+							description: val
+						});
+
+					}
+				})
+			], "bordered padded margined round"),
+			CT.dom.div([
+				"scenes",
+				_.scenes(game)
+			], "bordered padded margined round"),
+			CT.dom.div([
+				"conditions",
+				_.conditions(game)
+			], "bordered padded margined round"),
+			CT.dom.div([
+				"live",
+				CT.dom.checkboxAndLabel("list on games page",
+					game.live, null, null, null, function(cb) {
+						game.live = cb.checked;
+						vu.storage.edit({
+							key: game.key,
+							live: game.live
+						});
+					})
+			], "bordered padded margined round")
+		]);
+	},
+	init: function() {
 		CT.db.get("game", function(games) {
 			CT.modal.choice({
 				prompt: "please select a game",
 				data: ["new game"].concat(games),
 				cb: function(game) {
 					if (game == "new game")
-						_.create();
+						vu.builders.game.create();
 					else
-						_.load(game);
+						vu.builders.game.load(game);
 				}
 			});
 		}, null, null, null, {
