58934c8f2bd357c3db6ee7acf27657f0acb30806
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-10 20:32:09 -0700

    scene page loader

---
+++ ctvu/js/vu/pages/scene.js
@@ -0,0 +1,23 @@
+CT.require("CT.align");
+CT.require("CT.data");
+CT.require("CT.db");
+CT.require("CT.dom");
+CT.require("CT.key");
+CT.require("CT.layout");
+CT.require("CT.modal");
+CT.require("CT.parse");
+CT.require("CT.storage");
+CT.require("CT.trans");
+CT.require("core");
+CT.require("user.core");
+CT.require("zero.core");
+CT.require("vu.core");
+CT.require("vu.controls");
+CT.require("vu.storage");
+CT.require("vu.builders.scene");
+
+CT.onload(function() {
+	CT.initCore();
+	vu.core.init();
+	vu.storage.init(vu.builders.scene.menus, true);
+});
