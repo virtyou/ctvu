2da30dfc70841595fb79e66b14188be746e38102
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-10 20:58:20 -0700

    scene builder: menus (empty) - scripts, steps, actors, props; info menu w/ basic info - name, description, room

--- ctvu/js/vu/builders/scene.js
+++ ctvu/js/vu/builders/scene.js
@@ -1,15 +1,32 @@
 vu.builders.scene = {
 	_: {
 		selectors: {},
-		menus: {},
+		menus: {
+			info: "top",
+			scripts: "topleft",
+			steps: "bottomleft",
+			actors: "topright",
+			props: "bottomright"
+		},
+		load: function(scene) {
+			var _ = vu.builders.scene._, selz = _.selectors;
+			CT.dom.setContent(selz.info, [
+				CT.dom.div(scene.name, "bigger"),
+				scene.description,
+				"room: " + scene.room.name
+			]);
+		},
 		setup: function() {
 			var skey = location.hash.slice(1),
-				_ = vu.builders.scene._;
+				_ = vu.builders.scene._, selz = _.selectors;
 			if (!skey)
 				return alert("no scene specified!");
-			CT.db.one(skey, function(scene) {
-				
-			}, "json");
+			selz.info = CT.dom.div();
+			selz.scripts = CT.dom.div();
+			selz.steps = CT.dom.div();
+			selz.actors = CT.dom.div();
+			selz.props = CT.dom.div();
+			CT.db.one(skey, _.load, "json");
 		}
 	},
 	menus: function() {
