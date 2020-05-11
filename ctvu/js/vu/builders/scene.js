59f4593bae7d11da1ebd29ab402031c4d859f30c
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-10 22:13:17 -0700

    scene builder: scripts menu (needs: update all on swap [pending: all])

--- ctvu/js/vu/builders/scene.js
+++ ctvu/js/vu/builders/scene.js
@@ -9,15 +9,60 @@ vu.builders.scene = {
 			props: "bottomright"
 		},
 		load: function(scene) {
-			var _ = vu.builders.scene._, selz = _.selectors;
+			var _ = vu.builders.scene._, selz = _.selectors,
+				sname = CT.dom.span();
 			CT.dom.setContent(selz.info, [
 				CT.dom.div(scene.name, "bigger"),
 				scene.description,
-				"room: " + scene.room.name
+				"room: " + scene.room.name,
+				CT.dom.span("script:"),
+				CT.dom.pad(),
+				sname
 			]);
 			CT.dom.setContent(selz.actors, scene.actors.map(function(a) {
 				return a.name;
 			}));
+
+
+			vu.core.fieldList(selz.scripts, Object.keys(scene.scripts), null, function(v) {
+				var f = CT.dom.field(null, v);
+				if (v) {
+					f._trigger = v;
+					f.onfocus = function() {
+						sname.innerHTML = f._trigger;
+						//rz.refresh(f._trigger);
+					};
+					f.onkeyup = function() {
+						if (f.value) {
+							f.value = f.value.toLowerCase();
+							scene.scripts[f.value] = scene.scripts[f._trigger];
+							delete scene.scripts[f._trigger];
+							sname.innerHTML = f._trigger = f.value;
+							vu.storage.edit({
+								key: scene.key,
+								scripts: scene.scripts
+							});
+						} else
+							f.value = f._trigger; // meh
+					};
+				} else {
+					f.onkeyup = function() { f.value = f.value.toLowerCase() };
+				}
+				return f;
+			}, function(iput) {
+				var key = iput.value;
+				if (key in scene.scripts) return; // already exists...
+				scene.scripts[key] = [];
+				setTimeout(function() {
+					iput.focus();
+				});
+			}, function(val) {
+				delete scene.scripts[val];
+				vu.storage.edit({
+					key: scene.key,
+					scripts: scene.scripts
+				});
+			});
 		},
 		setup: function() {
 			var skey = location.hash.slice(1),
