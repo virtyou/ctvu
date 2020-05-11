ad56430518fb8790b5adf2d9dade41ea557fc992
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-11 00:11:29 -0700

    game.util: step(), extracted from script()

--- ctvu/js/vu/game/util.js
+++ ctvu/js/vu/game/util.js
@@ -12,29 +12,31 @@ vu.game.util = {
 				state[k] = v;
 		}
 	},
+	step: function(step, nextStep) {
+		var zcc = zero.core.current, r = zcc.room;
+		if (step.lights) {
+			step.lights.forEach(function(val, i) {
+				r.lights[i][step.directive || "setIntensity"](val);
+			});
+		}
+		if (step.camera)
+			zero.core.camera[step.camera](step.camopts);
+		if (step.prop)
+			r[step.prop][step.directive](step.direction);
+		if (step.state)
+			vu.game.util.upstate(state, step.state);
+		if (step.actor)
+			zcc.people[step.actor][step.action || "say"](step.line, nextStep);
+		else
+			nextStep();
+	},
 	script: function(script, cb, state) {
-		var step = script.shift(), nextStep,
-			zcc = zero.core.current, r = zcc.room;
-		if (step) {
-			nextStep = function() {
-				setTimeout(zero.core.util.script, step.pause || 0, script);
-			};
-			if (step.lights) {
-				step.lights.forEach(function(val, i) {
-					r.lights[i][step.directive || "setIntensity"](val);
-				});
-			}
-			if (step.camera)
-				zero.core.camera[step.camera](step.camopts);
-			if (step.prop)
-				r[step.prop][step.directive](step.direction);
-			if (step.state)
-				vu.game.util.upstate(state, step.state);
-			if (step.actor)
-				zcc.people[step.actor][step.action || "say"](step.line, nextStep);
-			else
-				nextStep();
-		} else
-			cb && cb();
+		var step = script.shift();
+		if (!step)
+			return cb && cb();
+		vu.game.util.step(step, function() {
+			setTimeout(zero.core.util.script, step.pause || 0,
+				script, cb, state);
+		});
 	}
 };
