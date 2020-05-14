ed09d5eba0b0be746837989506192ba754b840bd
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-13 21:29:25 -0700

    menu.Asset: call cb() etc

--- ctvu/js/vu/menu/Asset.js
+++ ctvu/js/vu/menu/Asset.js
@@ -1,19 +1,29 @@
 vu.menu.Asset = CT.Class({
 	CLASSNAME: "vu.menu.Asset",
 	_: {
-		ones: ["bg7.jpg", "bgBITZs4a.jpg", "bg_yan3.jpg", "bod.jpg", "bunny_ears.jpg", "bunny_teeth.jpg", "eye_brown_basic.jpg", "graph_paper.jpg", "hair_alphaGimp3_2SMALL.png", "hair_alphaGimp3a.png", "hairC5dHat.png", "hair.png", "hairShrunk.png", "head.jpg", "head_UV2.jpg", "icon.jpg", "room1.jpg", "shirt.jpg", "teeth256s.jpg", "white.jpg"],
 		textures: {},
+		ones: [
+			"bg7.jpg", "bgBITZs4a.jpg", "bg_yan3.jpg", "bod.jpg",
+			"bunny_ears.jpg", "bunny_teeth.jpg", "eye_brown_basic.jpg",
+			"graph_paper.jpg", "hair_alphaGimp3_2SMALL.png",
+			"hair_alphaGimp3a.png", "hairC5dHat.png", "hair.png",
+			"hairShrunk.png", "head.jpg", "head_UV2.jpg", "icon.jpg",
+			"room1.jpg", "shirt.jpg", "teeth256s.jpg", "white.jpg"
+		].map(function(o) {
+			return "/maps/one/" + o;
+		}),
 		sel: function(target) {
-			var _ = this._;
+			var _ = this._, cb = this.opts.cb;
 			return function() {
 				CT.modal.prompt({
 					prompt: "please select a texture",
 					style: "icon",
 					recenter: true,
 					className: "basicpopup mosthigh galimg",
-					data: _.ones.map(function(o) { return "/maps/one/" + o; }),
+					data: _.ones,
 					cb: function(tx) {
 						target.update({ texture: tx });
+						cb(tx);
 					}
 				});
 			};
@@ -31,6 +41,7 @@ vu.menu.Asset = CT.Class({
 		}
 	},
 	init: function(opts) {
+		this.opts = opts;
 		CT.db.get("asset", this.load, null, null, null, {
 			variety: "texture"
 		});
