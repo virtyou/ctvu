eca1d9f252812e8c3490edff7bc87b26c37317d3
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-13 23:43:51 -0700

    games page loader

---
+++ ctvu/js/vu/pages/games.js
@@ -0,0 +1,25 @@
+CT.require("CT.data");
+CT.require("CT.db");
+CT.require("CT.dom");
+CT.require("CT.layout");
+CT.require("CT.storage");
+CT.require("core");
+CT.require("user.core");
+
+CT.onload(function() {
+	CT.initCore();
+	CT.db.get("game", function(games) {
+		CT.dom.setContent("ctmain", [
+			CT.dom.div("games", "biggest centered"),
+			games.map(function(g) {
+				return CT.dom.link([
+					CT.dom.div(g.name, "big"),
+					g.description
+				], null, "/vu/adventure.html#" + g.key,
+					"bordered padded margined round inline-block hoverglow");
+			})
+		]);
+	}, null, null, null, {
+		live: true
+	});
+});
