81c07e4267e0dc95557657679d601aec816bc112
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-20 18:08:42 -0700

    gear page loader

---
+++ ctvu/js/vu/pages/gear.js
@@ -0,0 +1,28 @@
+CT.require("CT.align");
+CT.require("CT.data");
+CT.require("CT.dom");
+CT.require("CT.layout");
+CT.require("CT.modal");
+CT.require("CT.parse");
+CT.require("CT.storage");
+CT.require("CT.trans");
+CT.require("core");
+CT.require("user.core");
+CT.require("zero.core");
+CT.require("vu.core");
+CT.require("vu.controls");
+CT.require("vu.storage");
+CT.require("vu.menu.Body");
+CT.require("vu.builders.core");
+CT.require("vu.builders.gear");
+
+CT.onload(function() {
+	CT.initCore();
+	vu.core.init();
+	vu.storage.init(function() {
+		// menu
+		vu.builders.gear.menus();
+		// virtual world
+		vu.builders.core.init();
+	});
+});
