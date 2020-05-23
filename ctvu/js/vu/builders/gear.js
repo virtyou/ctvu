4a02489c58ba7c6fc0fe878da3e0eb866f38cd5e
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-23 00:36:30 -0700

    gear builder - builders.Gear: initMain(), loadMain(), swapper(), modder(); needs vu.media integration

--- ctvu/js/vu/builders/gear.js
+++ ctvu/js/vu/builders/gear.js
@@ -1,11 +1,50 @@
 vu.builders.Gear = CT.Class({
-	CLASSNAME: "vu.builders.Gear"
+	CLASSNAME: "vu.builders.Gear",
+	initMain: function() {
+		var p = zero.core.current.person;
+		p.opts.gear.worn = {};
+		p.opts.gear.held = {};
+		this.loadMain();
+	},
+	loadMain: function() {
+		this.setItem("gear", "worn", true);
+	},
+	swapper: function(modpart, part) {
+		if (modpart[part]) {
+			var tnode = CT.dom.div();
+			CT.db.one(modpart[part], function(fullp) {
+				CT.dom.setContent(tnode, fullp.name);
+			}, "json");
+			return [
+				tnode,
+				CT.dom.link("swap", function() {
+					// vu.media.....
+				}),
+				CT.dom.pad(),
+				CT.dom.link("adjust", function() {
+					// vu.media.....
+				})
+			];
+		} else {
+			return CT.dom.link("add", function() {
+				// vu.media.....
+			});
+		}
+	},
+	modder: function(sname, sitem, val, side, sub, part, axis, modpart) {
+		return CT.dom.div([
+			part,
+			this.swapper(modpart, part)
+		], "bordered padded margined round");
+	}
 }, vu.menu.Body);
 
 vu.builders.gear = {
 	menus: function() {
 		vu.builders.gear = new vu.builders.Gear({
-			main: "gear"
+			main: "gear",
+			secondary: "held",
+			impex: false
 		});
 	}
 };
