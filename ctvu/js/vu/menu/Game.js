a446591d5d60266f669665d051edc35c3d3983ec
bubbleboy14 <mario.balibrera@gmail.com>
2020-05-27 19:20:00 -0700

    menu.Game - various things, including persistent menus in general, including story menu: story(); _ - selectors{}, menus{story}, collapse(), basic(), setup(); constructor calls _.setup() and initializes persistent menus (story for now)

--- ctvu/js/vu/menu/Game.js
+++ ctvu/js/vu/menu/Game.js
@@ -1,6 +1,18 @@
 vu.menu.Game = CT.Class({
 	CLASSNAME: "vu.menu.Game",
 	_: {
+		selectors: {},
+		menus: {
+			story: "bottomleft"
+		},
+		collapse: function(section) {
+			var _ = this._, selz = _.selectors,
+				sel = selz[section];
+			return function() {
+				sel._collapsed = !sel._collapsed;
+				sel.modal.node.classList[sel._collapsed ? "add" : "remove"]("collapsed");
+			};
+		},
 		upper: function(variety, name) {
 			var scene = zero.core.current.scene;
 			this.log("upper():", variety, name);
@@ -12,18 +24,34 @@ vu.menu.Game = CT.Class({
 			vu.controls.setTriggers(n, this._.upper, person);
 			return n;
 		},
+		basic: function(name, side, info, cb) {
+			return vu.core.menu(name, side, info,
+				null, cb || this._.collapse(name));
+		},
 		info: function(name, info) {
 			var _ = this._, zc = zero.core;
 			if (_.infomenu)
 				_.infomenu.set([name, info]);
 			else {
-				_.infomenu = vu.core.menu(name, "bottom", info, null, function() {
+				_.infomenu = _.basic(name, "bottom", info, function() {
 					_.infomenu.hide();
 					zc.camera.follow(zc.current.person.body);
 				});
 			}
 			return _.infomenu;
+		},
+		setup: function() {
+			this._.selectors.story = CT.dom.div(null, "kidvp");
 		}
+	},
+	story: function() {
+		var _ = this._, selz = _.selectors,
+			mod = selz.story.modal, snode = mod.node;
+		CT.dom.setContent(selz.story, this.state.story);
+		mod.show();
+		if (snode.classList.contains("collapsed"))
+			snode.classList.remove("collapsed");
+		// TODO: scroll to bottom!
 	},
 	info: function(name, info, thing) {
 		this._.info(name, info).show();
@@ -45,9 +73,13 @@ vu.menu.Game = CT.Class({
 			], person.body);
 	},
 	init: function(opts) {
+		var _ = this._, s, selz = _.selectors;
 		this.opts = opts = CT.merge(opts, {
 			// ???
 		});
 		this.state = opts.state;
+		_.setup();
+		for (s in _.menus)
+			selz[s].modal = _.basic(s, _.menus[s], selz[s]);
 	}
 });
