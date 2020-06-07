4deec9df39a8a0c280a9c2c9663b0e8072770721
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-06 23:04:53 -0700

    portal: _{inject(),eject(),filter()}; improved portin() cur per check; unconditionalized _.inject()/eject() calls in port() and portin(); check() uses _.filter()

--- ctvu/js/vu/portal.js
+++ ctvu/js/vu/portal.js
@@ -1,28 +1,46 @@
 vu.portal = {
-	_: {},
+	_: {
+		inject: function(troom, pkey) { // join
+			var zcc = zero.core.current, person = zcc.person;
+			zero.core.util.room(CT.merge({
+				onbuild: function(room) {
+					room.inject(person,
+						pkey && zero.core.Thing.get(pkey));
+					room.cut();
+					person.body.show();
+					person.watch(false, true);
+				}
+			}, CT.data.get(troom || CT.storage.get("room"))));
+		},
+		eject: function(pkey) { // leave
+			var zcc = zero.core.current, person = zcc.person;
+			zcc.room.eject(person, pkey && zero.core.Thing.get(pkey));
+		},
+		filter: function(obj) {
+			var o = obj.opts, og = o.portals && o.portals.outgoing;
+			return og && og.target;
+		}
+	},
 	on: function(emission, cb) {
 		vu.portal._[emission] = cb;
 	},
 	portin: function(target, portin) {
 		var _ = vu.portal._, cur = zero.core.current;
 		Object.values(cur.people).forEach(function(person) {
-			if (!vu.core.ischar(person.opts.key))
+			if (person != cur.person)
 				person.remove();
 		});
-		_.inject && _.inject(target, portin);
+		_.inject(target, portin);
 	},
 	port: function(target, portout, portin) {
 		var vp = vu.portal, _ = vp._;
-		_.eject && _.eject(portout);
+		_.eject(portout);
 		setTimeout(vp.portin, 500, target, portin);
 	},
 	check: function() {
 		var cur = zero.core.current, person = cur.person,
-			pos = person.body.group.position, hit = false;
-		cur.room.objects.filter(function(obj) {
-			var o = obj.opts, og = o.portals && o.portals.outgoing;
-			return og && og.target;
-		}).forEach(function(portal) {
+			pos = person.body.group.position, _ = this._, hit = false;
+		cur.room.objects.filter(_.filter).forEach(function(portal) {
 			if (hit) return;
 			var dist = portal.position().distanceTo(pos);
 			CT.log(portal.name + " " + dist);
