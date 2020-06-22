4aa854144b1b9512d931820e657a247133801ce7
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-22 14:38:27 -0700

    tweak page loader: vu.hair import

--- ctvu/js/vu/pages/tweak.js
+++ ctvu/js/vu/pages/tweak.js
@@ -13,6 +13,7 @@ CT.require("user.core");
 CT.require("zero.core");
 CT.require("vu.core");
 CT.require("vu.color");
+CT.require("vu.hair");
 CT.require("vu.media");
 CT.require("vu.storage");
 CT.require("vu.builders.core");
