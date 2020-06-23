8b315ad59899afee37f7d3d4d1f004a3a06c0d7a
bubbleboy14 <mario.balibrera@gmail.com>
2020-06-22 17:23:28 -0700

    hair - support custom wild configurations: custom(); _.wild() and _.compile(); _.tweakables[]

--- ctvu/js/vu/hair.js
+++ ctvu/js/vu/hair.js
@@ -1,4 +1,99 @@
 vu.hair = {
+	_: {
+		tweakables: [{
+			name: "flex",
+			min: 0,
+			max: 2,
+			step: 0.1,
+			initial: 2
+		}, {
+			name: "kink",
+			min: 0,
+			max: 2,
+			step: 0.1,
+			initial: 0
+		}, {
+			name: "girth", // use defaults
+			initial: 3
+		}, {
+			name: "length",
+			min: 2,
+			max: 10,
+			step: 0.5,
+			initial: 6
+		}, {
+			name: "segments",
+			min: 3,
+			max: 10,
+			step: 1,
+			initial: 8
+		}, {
+			name: "taper_x",
+			min: 0.5,
+			max: 1.5,
+			step: 0.1,
+			initial: 0.8
+		}, {
+			name: "taper_y",
+			min: 0.5,
+			max: 1.5,
+			step: 0.1,
+			initial: 0.8
+		}, {
+			name: "taper_z",
+			min: 0.5,
+			max: 1.5,
+			step: 0.1,
+			initial: 0.8
+		}, {
+			name: "coverage_x",
+			min: 0.1,
+			max: 1,
+			step: 0.1,
+			initial: 1
+		}, {
+			name: "coverage_z",
+			min: 0.1,
+			max: 1,
+			step: 0.1,
+			initial: 1
+		}, {
+			name: "density",
+			min: 2,
+			max: 9,
+			initial: 7
+		}],
+		wild: function(opts, name) {
+			return CT.merge(opts, {
+				thing: "Hair",
+				name: name || "wild",
+				kind: "hair",
+				bone: 4
+			});
+		},
+		compile: function(opts) {
+			var coverage = [opts.coverage_x, opts.coverage_z],
+				p, density = opts.density;
+			opts.taper = [opts.taper_x, opts.taper_y, opts.taper_z];
+			for (p of ["x", "y", "z"])
+				delete opts["taper_" + p];
+			for (p of ["coverage_x", "coverage_z", "density"])
+				delete opts[p];
+			return vu.hair._.wild({
+				strand: opts,
+				density: density,
+				coverage: coverage
+			});
+		}
+	},
+	custom: function() {
+		CT.modal.prompt({
+			prompt: "tweak away!",
+			style: "form",
+			numbers: vu.hair._.tweakables,
+			cb: vz => vu.hair.attach(vu.hair._.compile(vz))
+		});
+	},
 	attach: function(hnew) {
 		var head = vu.hair.person.head;
 		head.detach("hair");
@@ -13,13 +108,8 @@ vu.hair = {
 			data: Object.keys(zbbh).concat("custom"),
 			cb: function(hvar) {
 				if (hvar == "custom")
-					return; // TODO .....
-				vu.hair.attach(CT.merge({
-					thing: "Hair",
-					name: hvar,
-					kind: "hair",
-					bone: 4
-				}, zbbh[hvar]));
+					return vu.hair.custom();
+				vu.hair.attach(vu.hair._.wild(zbbh[hvar], hvar));
 			}
 		});
 	},
