78c4c41fb7aa022b189dcdf6b40c3ad7662bff60
bubbleboy14 <mario.balibrera@gmail.com>
2020-07-13 14:58:18 -0700

    games page loader - CT imports: align, modal, trans

--- ctvu/js/vu/pages/games.js
+++ ctvu/js/vu/pages/games.js
@@ -1,8 +1,11 @@
+CT.require("CT.align");
 CT.require("CT.data");
 CT.require("CT.db");
 CT.require("CT.dom");
 CT.require("CT.layout");
+CT.require("CT.modal");
 CT.require("CT.storage");
+CT.require("CT.trans");
 CT.require("core");
 CT.require("user.core");
 
