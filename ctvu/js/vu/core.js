0cf103cf82f330cbc000847a74222afa98e1f1cc
bubbleboy14 <mario.balibrera@gmail.com>
2020-08-25 13:05:32 -0700

    vu.core: impconf() and base(), extracted/elaborated from, and now used by, charlinx(); needs basepack/basepacks stuff

--- ctvu/js/vu/core.js
+++ ctvu/js/vu/core.js
@@ -206,6 +206,54 @@ vu.core = {
 			}
 		}, null, null, exporter);
 	},
+	impconf: function() {
+		var data = vu.core._udata, person = data.person;
+		CT.modal.choice({
+			prompt: "import configuration (morphs, vibe, gestures, dances, responses) from whom?",
+			data: data.people.filter(function(peep) {
+				return peep.name != person.name;
+			}),
+			cb: function(whom) {
+				vu.core.v({
+					action: "import",
+					from: whom.key,
+					to: person.key
+				}, function() {
+					window.location = location; // oh my, what a hack!
+				});
+			}
+		});
+	},
+	base: function() {
+		var data = vu.core._udata, person = data.person,
+			choices = [], ioin = person.basepack.length ? "is" : "is not";
+		if (data.people.length > 1) // impconf()
+			choices.push("import configuration");
+		if (!person.basepacks.length)
+			choices.push("register/edit basepack");
+		if (!person.basepack.length) // TODO: if basepacks available...
+			choices.push("assign/reorder basepacks");
+		CT.modal.choice({
+			prompt: CT.dom.div([
+				CT.dom.div("basepacks and configuration", "bigger centered"),
+				"The behavior and appearance of your characters are defined across a variety of configuration blocks, including: mood, vibe, mods, gear, dances, gestures, and responses.",
+				"You may freely pass configuration blocks between your characters (see 'import configuration').",
+				"Additionally, you have the option of sharing aspects of your characters' configuration as a basepack ('register/edit basepack'), which essentially is a public character configuration.",
+				"Alternatively, you may incorporate other basepacks ('assign/reorder basepacks') into your character.",
+				"In summary:",
+				"- If you have more than one character, you can do a full configuration import.",
+				"- If you do not have any basepacks assigned to this character, you can register this character as a basepack (composed of the configuration blocks you select).",
+				"- If this character isn't registered as a basepack, you can assign basepacks. Note that order matters, as basepacks lower in the list overwrite overlapping configuration bits of higher-up basepacks.",
+				"This character, " + person.name + ", " + ioin + " registered as a basepack."
+			], "kidvp"),
+			data: choices,
+			cb: function(sel) {
+				if (sel == "import configuration")
+					return vu.core.impconf();
+				// TODO: basepack/basepacks
+			}
+		});
+	},
 	charlinx: function() {
 		var data = vu.core._udata, person = data.person, nodes = vu.core.share(person, [
 			CT.dom.link("swap", function() {
@@ -214,28 +262,10 @@ vu.core = {
 				});
 			})
 		]);
-		if (data.people.length > 1) {
-			nodes = nodes.concat([
-				CT.dom.pad(),
-				CT.dom.link("import", function() {
-					CT.modal.choice({
-						prompt: "import configuration (morphs, vibe, gestures, dances, responses) from whom?",
-						data: data.people.filter(function(peep) {
-							return peep.name != person.name;
-						}),
-						cb: function(whom) {
-							vu.core.v({
-								action: "import",
-								from: whom.key,
-								to: person.key
-							}, function() {
-								window.location = location; // oh my, what a hack!
-							});
-						}
-					});
-				})
-			]);
-		}
+		nodes = nodes.concat([
+			CT.dom.pad(),
+			CT.dom.link("base", vu.core.base)
+		]);
 		return CT.dom.div([[
 			CT.dom.span("hello,"),
 			CT.dom.pad(),
