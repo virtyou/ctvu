c37ff27a07f85cb749478a3da76d27386187b6a5
bubbleboy14 <mario.balibrera@gmail.com>
2021-02-28 19:56:45 -0800

    voice module: _.resume() and _.action(); parse() lowers and log()s phrase, and uses _.action() and _.resume(); listen() uses resume()

--- ctvu/js/vu/voice.js
+++ ctvu/js/vu/voice.js
@@ -5,6 +5,25 @@ vu.voice = {
 		mode: "walk", // walk|look|dance
 		cams: ["pov", "behind", "cycle"],
 		personal: ["say", "approach", "dance", "gesture", "mod", "vibe"],
+		resume: function() {
+			var _ = vu.voice._;
+			_.listening && zero.core.rec.listen(vu.voice.parse, _.resume);
+		},
+		action: function(action, target) {
+			var zcc = zero.core.current, p = zcc.person, checker;
+			if (action == "vibe")
+				checker = p.opts.vibe;
+			else if (action == "approach")
+				checker = zcc.people;
+			else if (action != "say")
+				checker = p.opts[action + "s"];
+			if (checker && !(target in checker))
+				p.say("i can't " + action + " " + target);
+			else if (action == "vibe")
+				p.vibe.update(target);
+			else
+				p[action](target);
+		}
 	},
 	commands: {
 		mode: function(mode) {
@@ -19,9 +38,11 @@ vu.voice = {
 		}
 	},
 	parse: function(phrase) {
+		phrase = phrase.toLowerCase();
 		var _ = vu.voice._, comz = vu.voice.commands,
 			words = phrase.split(" "), action = words.shift(),
 			rest = words.join(" "), p = zero.core.current.person;
+		CT.log(phrase);
 		if (_.convo) {
 			if (phrase == "end conversation")
 				_.convo = false;
@@ -30,19 +51,19 @@ vu.voice = {
 		} else if (phrase == "start conversation")
 			_.convo = true;
 		else if (_.personal.includes(action))
-			p[action](rest);
+			_.action(action, rest);
 		else if (action in comz)
 			comz[action](rest);
 		else
 			CT.log("no match: " + phrase);
-		if (_.listening)
-			zero.core.rec.listen(vu.voice.parse);
+		_.resume();
 	},
 	unlisten: function() {
 		vu.voice._.listening = false;
 	},
 	listen: function() {
-		vu.voice._.listening = true;
-		zero.core.rec.listen(vu.voice.parse);
+		var _ = vu.voice._;
+		_.listening = true;
+		_.resume();
 	}
 };
