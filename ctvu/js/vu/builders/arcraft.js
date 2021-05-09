6d8a3983b5da4e4dbd3c7993275a9f1cc60b043f
bubbleboy14 <mario.balibrera@gmail.com>
2021-05-09 16:51:37 -0700

    skeletal arcraft builder

---
+++ ctvu/js/vu/builders/arcraft.js
@@ -0,0 +1,40 @@
+vu.builders.arcraft = {
+	_: {
+		selectors: {},
+		menus: {
+			basic: "topleft",
+			anchors: "topright",
+			lights: "bottomleft"
+		},
+		setup: function() {
+			var _ = vu.builders.arcraft._, selz = _.selectors, section;
+			for (section in _.menus)
+				selz[section] = CT.dom.div();
+		},
+		swap: function() {
+
+		},
+		linx: function() {
+			var _ = vu.builders.arcraft._;
+			_.sharer = vu.core.sharer();
+			_.curname = CT.dom.span(null, "bold");
+			return CT.dom.div([
+				[
+					CT.dom.span("viewing:"),
+					CT.dom.pad(),
+					_.curname
+				], [
+					CT.dom.link("swap", _.swap),
+					CT.dom.pad(),
+					_.sharer
+				]
+			], "left shiftall");
+		}
+	},
+	menus: function() {
+		var section, _ = vu.builders.test._, selz = _.selectors;
+		_.setup();
+		for (section in _.menus)
+			vu.core.menu(section, _.menus[section], selz[section]).show("ctmain");
+	}
+};
