94eee14b57d11a844aed2d3c63dfb3ba4a5da4be
bubbleboy14 <mario.balibrera@gmail.com>
2021-05-09 16:44:27 -0700

    basic arcraft loader

---
+++ ctvu/js/vu/pages/arcraft.js
@@ -0,0 +1,28 @@
+CT.require("CT.align");
+CT.require("CT.data");
+CT.require("CT.db");
+CT.require("CT.dom");
+CT.require("CT.layout");
+CT.require("CT.modal");
+CT.require("CT.parse");
+CT.require("CT.storage");
+CT.require("CT.trans");
+CT.require("core");
+CT.require("user.core");
+CT.require("zero.core");
+CT.require("vu.core");
+CT.require("vu.controls");
+CT.require("vu.storage");
+CT.require("vu.builders.core");
+CT.require("vu.builders.arcraft");
+
+CT.onload(function() {
+	CT.initCore();
+	vu.core.init();
+	vu.storage.init(function() {
+		// menu
+		vu.builders.arcraft.menus();
+		// virtual world
+		vu.builders.core.init();
+	});
+});
