f5d57c3f3a86d9fa3f127514ef8b41c97d1d0594
bubbleboy14 <mario.balibrera@gmail.com>
2021-05-09 20:06:13 -0700

    arcraft builder, _: load(); craft(); getAugmentations() (now called from linx()); menus{} rename, anchors->markers

--- ctvu/js/vu/builders/arcraft.js
+++ ctvu/js/vu/builders/arcraft.js
@@ -3,7 +3,7 @@ vu.builders.arcraft = {
 		selectors: {},
 		menus: {
 			basic: "topleft",
-			anchors: "topright",
+			markers: "topright",
 			lights: "bottomleft"
 		},
 		setup: function() {
@@ -11,13 +11,50 @@ vu.builders.arcraft = {
 			for (section in _.menus)
 				selz[section] = CT.dom.div();
 		},
+		load: function(aug) {
+			var _ = vu.builders.arcraft._, selz = _.selectors;
+			_.sharer.update(aug);
+			CT.dom.setContent(_.curname, aug.name);
+		},
 		swap: function() {
 
+		},
+		craft: function() {
+			var _ = vu.builders.arcraft._;
+			CT.modal.prompt({
+				prompt: "what's the new augmentation's name?",
+				cb: function(name) {
+					vu.core.v({
+						action: "augmentation",
+						owners: [user.core.get("key")],
+						name: name
+					}, function(item) {
+						_.augs.push(item);
+						_.swap(item);
+					});
+				}
+			});
+		},
+		getAugmentations: function() {
+			var _ = vu.builders.arcraft._;
+			CT.db.get("augmentation", function(augs) {
+				_.augs = augs;
+				if (augs.length)
+					_.swap(augs[0]);
+				else
+					_.craft();
+			}, 1000, null, null, {
+				owners: {
+					comparator: "contains",
+					value: user.core.get("key")
+				}
+			});
 		},
 		linx: function() {
 			var _ = vu.builders.arcraft._;
 			_.sharer = vu.core.sharer();
 			_.curname = CT.dom.span(null, "bold");
+			_.getAugmentations();
 			return CT.dom.div([
 				[
 					CT.dom.span("viewing:"),
