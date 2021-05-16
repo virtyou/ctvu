0d2b0ff386599a244cf06ccb1a4251de076783df
bubbleboy14 <mario.balibrera@gmail.com>
2021-05-16 16:54:28 -0700

    builders.core, init(), arcraft: room.gravity false; cam z 20->10

--- ctvu/js/vu/builders/core.js
+++ ctvu/js/vu/builders/core.js
@@ -6,11 +6,13 @@ vu.builders.core = {
 		isiora && ["environment", "texture", "objects", "obstacle", "floor", "wall", "ramp"].forEach(function(item) {
 			delete core.config.ctzero.room[item];
 		});
-		if (pname == "arcraft")
+		if (pname == "arcraft") {
 			delete core.config.ctzero.room.lights;
+			core.config.ctzero.room.gravity = false;
+		}
 		zero.core.util.init();
 		isiora && zero.core.camera.move({
-			x: 0, y: 0, z: pname == "item" && 200 || 20
+			x: 0, y: 0, z: pname == "item" && 200 || 10
 		});
 		if (isiora || pname == "zone")
 			CT.dom.addContent("ctheader", _.linx());
