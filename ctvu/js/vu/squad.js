b481a78521680b6141b4c27879ba42b146502dde
bubbleboy14 <mario.balibrera@gmail.com>
2023-03-30 20:32:28 -0700

    squad: chans(), now used by switch(); roomvite() and roomvite() support in mod()

--- ctvu/js/vu/squad.js
+++ ctvu/js/vu/squad.js
@@ -39,12 +39,17 @@ vu.squad = {
 			}
 		});
 	},
-	switch: function() {
-		var _ = vu.squad._, basechans = ["room", "global"];
+	chans: function(noroom) {
+		var _ = vu.squad._, basechans = ["global"];
+		noroom || basechans.unshift("room");
 		user.core.get("admin") && basechans.push("admin");
+		return basechans.concat(_.squads);
+	},
+	switch: function() {
+		var _ = vu.squad._;
 		CT.modal.choice({
 			prompt: "which channel do you want to talk in?",
-			data: basechans.concat(_.squads),
+			data: vu.squad.chans(),
 			cb: function(chan) {
 				_.current = chan;
 			}
@@ -58,11 +63,24 @@ vu.squad = {
 			cb: vu.live.invite
 		});
 	},
+	roomvite: function() {
+		var _ = vu.squad._, squads;
+		if (_.current != "room")
+			return vu.live.roomvite(_.current);
+		squads = vu.squad.chans(true);
+		if (squads.length == 1)
+			return vu.live.roomvite("global");
+		CT.modal.choice({
+			prompt: "which squad are you telling?",
+			data: squads,
+			cb: vu.live.roomvite
+		});
+	},
 	mod: function(e) {
-		var _ = vu.squad._, chans = ["switch channels", "join squad"];
+		var _ = vu.squad._, chans = ["switch channels", "join squad", "send room invite"];
 		e.stopPropagation();
 		if (_.squads.length)
-			chans = chans.concat(["send invite", "quit squad"]);
+			chans = chans.concat(["send squad invite", "quit squad"]);
 		CT.modal.choice({
 			prompt: "you're speaking to the " + _.current + " channel",
 			data: chans,
@@ -71,10 +89,12 @@ vu.squad = {
 					vu.squad.switch();
 				else if (action == "join squad")
 					vu.squad.joiner();
+				else if (action == "send room invite")
+					vu.squad.roomvite();
+				else if (action == "send squad invite")
+					vu.squad.invite();
 				else if (action == "quit squad")
 					vu.squad.quit();
-				else // send invite
-					vu.squad.invite();
 			}
 		});
 	},
