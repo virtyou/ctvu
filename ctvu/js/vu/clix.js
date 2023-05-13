6d42923f96fc45b574173eaca443054fed3515e5
bubbleboy14 <mario.balibrera@gmail.com>
2023-05-12 20:54:34 -0700

    clix: refined via _.yinfo() and info(), which falls back (if !opts.info node) to using zcc.adventure.menus.basic() w/ glowy true

--- ctvu/js/vu/clix.js
+++ ctvu/js/vu/clix.js
@@ -1,4 +1,24 @@
 vu.clix = {
+	_: {
+		yinfo: function() {
+			var oz = vu.clix.opts;
+			return [
+				CT.dom.div("(you)", "up20 right"),
+				"move around with WASD",
+				"rotate with Q and E",
+				"adjust the camera with ARROWS",
+				"zoom with PERIOD and COMMA",
+				"SPACE for jump",
+				"SHIFT for run",
+				"ENTER to enter portal",
+				"1-9 for gestures",
+				"1-9 + SHIFT for dances",
+				"0 to ungesture",
+				"0 + SHIFT to undance",
+				oz.streamer && oz.streamer()
+			];
+		}
+	},
 	action: function() {
 		// TODO: other actions.....
 		vu.portal.check();
@@ -12,12 +32,26 @@ vu.clix = {
 		vu.help.triggerize(p.brain, vu.clix.opts.auto, vu.squad.emit);
 		vu.clix.register(p);
 	},
+	info: function(name, description) {
+		var inode = this.opts.info, zcc = zero.core.current;
+		if (!inode) {
+			CT.log("vu.clix.info(): opts.info node not found - using Game Menu");
+			zcc.adventure.menus.basic(name, description, true);
+		} else {
+			CT.dom.setContent(inode, [
+				vu.controls.help("info"),
+				CT.dom.div(name, "bigger"),
+				description
+			]);
+			CT.trans.glow(inode);
+		}
+		zero.core.audio.ux("blipon");
+	},
 	register: function(thing) {
-		var zc = zero.core, other = [
+		var zc = zero.core, vc = vu.clix, other = [
 			"SHIFT + click to approach"
 		], zcc = zc.current, cam = zc.camera,
-			target = thing.body || thing,
-			oz = vu.clix.opts, inode = oz.inode,
+			target = thing.body || thing, oz = vc.opts,
 			isYou = vu.core.ischar(thing.opts.key);
 		if (thing.body) {
 			if (thing.automaton)
@@ -32,29 +66,9 @@ vu.clix = {
 		else if (thing.opts.kind == "carpentry" && thing.opts.items.length)
 			other.push(thing.perusebutt());
 		else if (thing.opts.kind == "portal")
-			other.push(CT.dom.button("enter", () => zcc.person.approach(thing, vu.clix.action)));
+			other.push(CT.dom.button("enter", () => zcc.person.approach(thing, vc.action)));
 		zc.click.register(target, function() {
-			CT.dom.setContent(inode, [
-				vu.controls.help("info"),
-				CT.dom.div(thing.name, "bigger"),
-				isYou ? [
-					CT.dom.div("(you)", "up20 right"),
-					"move around with WASD",
-					"rotate with Q and E",
-					"adjust the camera with ARROWS",
-					"zoom with PERIOD and COMMA",
-					"SPACE for jump",
-					"SHIFT for run",
-					"ENTER to enter portal",
-					"1-9 for gestures",
-					"1-9 + SHIFT for dances",
-					"0 to ungesture",
-					"0 + SHIFT to undance",
-					oz.streamer && oz.streamer()
-				] : other
-			]);
-			zc.audio.ux("blipon");
-			CT.trans.glow(inode);
+			vc.info(thing.name, isYou ? vc._.yinfo() : other);
 			cam.follow(target.looker || target);
 			if (!isYou) {
 				target.playPause(oz.audup);
@@ -62,7 +76,7 @@ vu.clix = {
 			}
 		});
 	},
-	init: function(opts) { // {inode,streamer,audup,auto}
-		this.opts = opts;
+	init: function(opts) { // {info,streamer,audup,auto}
+		vu.clix.opts = opts;
 	}
 };
