bdd3a7bf806c0b72b758dfcf976e3ce06ed79e31
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-02 00:23:20 -0700

    game.Boss: basic Meter

--- ctvu/js/vu/game/Boss.js
+++ ctvu/js/vu/game/Boss.js
@@ -7,6 +7,14 @@ vu.game.Boss = CT.Class({
 	},
 	shove: function(direction, amount) {
 		this.person.body.shove(direction, amount);
+		this.hit();
+	},
+	hit: function(amount) {
+		this.hp -= (amount || 1);
+		this.meter.set(this.hp);
+		if (this.hp < 1) { // TODO: actual defeat...
+			this.log("defeated!!!!!");
+		}
 	},
 	scale: function(s) {
 		this.person.body.scale(s, true);
@@ -20,6 +28,7 @@ vu.game.Boss = CT.Class({
 	wileOut: function() {
 		this.climax = true;
 		this.drop();
+		this.meter.show();
 	},
 	setLevel: function(level) {
 		this.log("level", level);
@@ -45,12 +54,44 @@ vu.game.Boss = CT.Class({
 			level: 1,
 			floor: 1,
 			cap: 5,
-			hp: 10
+			hp: 100
 		});
 		if (!opts.person)
 			opts.person = zero.core.current.people[opts.name];
+		this.hp = opts.hp;
 		this.person = opts.person;
 		this.setLevel(opts.level);
 		this.person.body.oncrash = opts.oncrash;
+		this.meter = new vu.game.Boss.Meter({ hp: opts.hp });
+	}
+});
+
+vu.game.Boss.Meter = CT.Class({
+	CLASSNAME: "vu.game.Boss.Meter",
+	_: {
+		width: function() {
+			return (this.hp * this.opts.unit) + "px";
+		}
+	},
+	set: function(hp) {
+		this.hp = Math.max(hp, 0);
+		this.bloodLine.style.width = this._.width();
+	},
+	show: function() {
+		this.menu.show("ctmain");
+	},
+	initMenu: function() {
+		this.bloodLine = CT.dom.div(null, "h40p redback");
+		this.lifeLine = CT.dom.div(this.bloodLine, "bordered noflow");
+		this.bloodLine.style.width = this.lifeLine.style.width = this._.width();
+		this.menu = vu.core.menu("battle", "top", this.lifeLine);
+	},
+	init: function(opts) {
+		this.opts = opts = CT.merge(opts, {
+			hp: 100,
+			unit: 4
+		});
+		this.hp = opts.hp;
+		this.initMenu();
 	}
 });
