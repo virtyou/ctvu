1ea27ab07005e2956dd4a49dc1ee4b5a4927993e
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-07 17:14:02 -0700

    Meter - light refactor

--- ctvu/js/vu/game/Meter.js
+++ ctvu/js/vu/game/Meter.js
@@ -2,30 +2,41 @@ vu.game.Meter = CT.Class({
 	CLASSNAME: "vu.game.Meter",
 	_: {
 		width: function() {
-			return (this.value * this.opts.unit) + "px";
+			return (this.value / this.opts.cap) * 100 + "%";
 		}
 	},
-	set: function(val) {
+	status: function() {
+		return this.value + "/" + this.opts.cap;
+	},
+	set: function(val, cap) {
 		this.value = Math.max(val, 0);
-		this.bloodLine.style.width = this._.width();
+		if (cap)
+			this.opts.cap = isNaN(cap) ? Math.max(val, this.opts.cap) : cap;
+		this.counter.style.width = this._.width();
 	},
 	show: function() {
-		this.menu.show("ctmain");
+		this.menu ? this.menu.show("ctmain") : CT.dom.show(this.line);
+	},
+	hide: function() {
+		this.menu ? this.menu.hide() : CT.dom.hide(this.line);
 	},
 	initMeter: function() {
-		this.bloodLine = CT.dom.div(null, "h15p redback");
-		this.lifeLine = CT.dom.div(this.bloodLine, "bordered noflow");
-		this.bloodLine.style.width = this.lifeLine.style.width = this._.width();
+		const oz = this.opts;
+		this.counter = CT.dom.div(null, oz.counterClass);
+		this.line = CT.dom.div(this.counter, "bordered noflow");
+		this.counter.style.width = this._.width();
+		this.line.style.width = oz.lineWidth;
 		if (this.opts.menu)
-			this.menu = vu.core.menu("battle", "top", this.lifeLine);
+			this.menu = vu.core.menu("battle", "top", this.line);
 	},
 	init: function(opts) {
 		this.opts = opts = CT.merge(opts, {
 			cap: 100,
-			unit: 4,
-			menu: false
+			menu: false,
+			lineWidth: "100%",
+			counterClass: "h15p redback"
 		});
-		this.value = opts.value || opts.cap;
+		this.value = isNaN(opts.value) ? opts.cap : opts.value;
 		this.initMeter();
 	}
 });
