e1e75d103b2a913094b02cab47bf7f0787aaa9ec
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-27 18:07:20 -0700

    scene page loader: added vu.build.core import

--- ctvu/js/vu/pages/scene.js
+++ ctvu/js/vu/pages/scene.js
@@ -22,6 +22,7 @@ CT.require("vu.game.util");
 CT.require("vu.game.step");
 CT.require("vu.game.stepper");
 CT.require("vu.game.hopper");
+CT.require("vu.build.core");
 CT.require("vu.builders.scene");
 
 CT.onload(function() {
