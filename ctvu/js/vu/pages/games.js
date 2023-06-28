00c9fecd9d4bd9fc7134d826ce658d4d21dfa06e
bubbleboy14 <mario.balibrera@gmail.com>
2023-06-27 17:26:14 -0700

    games page loader: parse require()

--- ctvu/js/vu/pages/games.js
+++ ctvu/js/vu/pages/games.js
@@ -4,6 +4,7 @@ CT.require("CT.db");
 CT.require("CT.dom");
 CT.require("CT.layout");
 CT.require("CT.modal");
+CT.require("CT.parse");
 CT.require("CT.storage");
 CT.require("CT.trans");
 CT.require("core");
